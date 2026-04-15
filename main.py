4f350162ddf89293cbc5f225f19c0bc9af594602
student <lovereun101900@naver.com>
2026-04-15 21:45:10 +0900

    점수 비교 기능 구현

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ class NPUSimulator:
     
     @staticmethod
     def calculate_mac(pattern, filter_matrix):
-        # [COMMIT: MAC 연산 (외부 라이브러리 금지, 반복문 구현)]
+
         n = len(pattern)
         mac_score = 0.0
         for i in range(n):
             for j in range(n):
                 mac_score += pattern[i][j] * filter_matrix[i][j]
         return mac_score
+    
+    @staticmethod
+    def judge(score_a, score_b, label_a='A', label_b='B'):
+
+        if abs(score_a - score_b) < EPSILON:
+            return 'UNDECIDED'
+        return label_a if score_a > score_b else label_b
